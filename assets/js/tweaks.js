599eaeb9697e84d20a441e16a92e485f18af60ae
estds <56233807+estds@users.noreply.github.com>
2020-04-09 21:44:49 +0800

    Update tweaks.js

--- assets/js/tweaks.js
+++ assets/js/tweaks.js
@@ -131,21 +131,16 @@ $(document).scroll(function() {
     if (!fixed) {
       fixed = true;
       // $('#to-top').css({position:'fixed', display:'block'});
-      $('#to-top').show("slow", function() {
-        $('#to-top').css({
-          position: 'fixed',
+      $('#to-top').css({
           display: 'block'
         });
-      });
     }
   } else {
     if (fixed) {
       fixed = false;
-      $('#to-top').hide("slow", function() {
-        $('#to-top').css({
+      $('#to-top').css({
           display: 'none'
         });
-      });
     }
   }
 });
