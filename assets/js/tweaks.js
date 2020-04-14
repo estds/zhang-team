5c75902bd752e73dfb169505af025e6c2c012c23
estds <56233807+estds@users.noreply.github.com>
2020-04-14 22:32:32 +0800

    Update tweaks.js

--- assets/js/tweaks.js
+++ assets/js/tweaks.js
@@ -169,7 +169,7 @@ $(function() {
       classNames: {
         menu: 'w-100',
         hint: 'text-muted',
-        dataset: 'dropdown-menu show shadow mt-1 w-100',
+        dataset: 'typeahead-dropdown dropdown-menu show shadow mt-1 w-100',
         //suggestion: 'dropdown-item small text-truncate',
         highlight: 'amber lighten-4',
       },
